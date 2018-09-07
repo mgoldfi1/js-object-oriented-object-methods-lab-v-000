2ffde84dd615a4aad00a90a804cf974a04d806ab
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 16:50:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function BoardMember (name, homeState, training){
+function BoardMember (nam, homeState, training){
   this.homeState = homeState;
-  this.name = name;
+  this.nam = name;
   this.training = training;
   this.veto = function() {return "No, I must disagree"};
   this.approve = function(){return "You can do that!"};
   this.doCharity = function(){return "I like to help people."};
   this.releasePressStatement = function(){return "You will see great things from Scuber."};
-  this.sayHi = function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`};
+  this.sayHi = function(){return `Hi, my name is ${this.nam}. I am from ${this.homeState}, and I was trained in ${this.training}`};
 }
-let john = new BoardMember("john","NY","none");
-console.log(john.name)
+
