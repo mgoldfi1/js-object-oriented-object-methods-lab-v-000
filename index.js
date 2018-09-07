834091e2b35f5fcddf4625c2fd99e13218fdf6c4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 16:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ function BoardMember (name, homeState, training){
   this.releasePressStatement = function(){return "You will see great things from Scuber."};
   this.sayHi = function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`};
 }
+let john = new BoardMember("john","NY","none");
+console.log(john.name)
