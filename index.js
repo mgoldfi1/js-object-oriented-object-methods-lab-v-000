2d98b164112a37c6a59301761a238da9151879e7
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 16:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function BoardMember(name,homeState,training) {
-  this.name = name 
+  this.name = name
   this.homeState = homeState
-  this.training = training 
+  this.training = training
   this.veto = function() {return "No, I must disagree"}
+  this.approve = function(){return "You can do that!"}
 }
